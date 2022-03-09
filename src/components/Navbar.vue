f239ce123ea4ac913a8c1a6e54587854a1868e2d
WernervanderMerwe <wernervandermerwe@rocketmail.com>
2022-03-09 19:02:59 +0200

    navbar component commit

---
+++ src/components/Navbar.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="grid grid-cols-3 border-b content-center">
+    <div class="w-28 m-4 p-1 my-auto col-span-2">
+      <img alt="bOnline logo" src="./../assets/bOnline-logo.svg" />
+    </div>
+    <div class="m-4 col-end-4 flex">
+      <div class="bellIcon border-r mr-4 my-auto bg-slate-300 w-8 h-8"></div>
+      <div class="accountUser flex flex-row border-l pl-4">
+        <div
+          class="accountUserIcon rounded-full border my-auto h-8 w-8 bg-gray-300"
+        ></div>
+        <div class="ml-1 flex flex-col">
+          <p class="font-bold">{{ firstLastName }}</p>
+          <p class="font-light">firstlastname@bonline.com</p>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "AppNavbar",
+  data() {
+    return {
+      firstLastName: "First Lastname data",
+    };
+  },
+};
+</script>
