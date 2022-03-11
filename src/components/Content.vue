3aa4c65eeaf0a8730387d231fb19ea0ba4c204cf
WernervanderMerwe <wernervandermerwe@rocketmail.com>
2022-03-11 17:00:10 +0200

    accounts row DRY commit

--- src/components/Content.vue
+++ src/components/Content.vue
@@ -4,7 +4,7 @@
       <p class="font-bold text-2xl">Billing</p>
       <p>Overview of your accounts</p>
     </div>
-    <content-tabs></content-tabs>
+    <content-tabs :jsonData="data"></content-tabs>
     <div class="grid grid-cols-3 px-6 gap-4 pt-4">
       <payment-details></payment-details>
       <my-products></my-products>
@@ -19,8 +19,15 @@ import PaymentDetails from "./PaymentDetails.vue";
 import MyProducts from "./MyProducts.vue";
 import BillingHistory from "./BillingHistory.vue";
 
+import data from "./../../instructions/Data.json";
+
 export default {
   name: "appContent",
+  data() {
+    return {
+      data: data,
+    };
+  },
   components: {
     contentTabs: ContentTabs,
     paymentDetails: PaymentDetails,
--- src/components/ContentTabs.vue
+++ src/components/ContentTabs.vue
@@ -1,17 +1,27 @@
 <template>
   <div class="px-6 py-2 border-b flex row-auto gap-2 h-20">
-    <div class="border rounded-md p-1 px-2 bg-white">
+    <div
+      class="border rounded-md p-1 px-2 bg-white"
+      v-for="account in jsonData"
+      :key="account.account_id"
+    >
       <div class="flex row-auto gap-2">
-        <p class="font-bold text-blue-400">Macrotel Phone</p>
+        <p class="font-bold text-blue-400">{{ account.business_name }}</p>
         <div
-          class="border rounded-full px-2 text-xs font-bold my-auto text-green-300"
+          class="border rounded-full px-2 text-xs font-semibold my-auto"
+          :class="{
+            'text-green-400': account.account_status === 'Live',
+            'text-yellow-400': account.account_status === 'Suspended',
+            'text-red-400': account.account_status === 'Cancelled',
+          }"
         >
-          Active
+          {{ account.account_status }}
         </div>
       </div>
-      <p class="text-xs leading-6">Account ID : 63531691</p>
+      <p class="text-xs leading-6">Account ID : {{ account.account_id }}</p>
     </div>
-    <div class="border rounded-md p-1 px-2 bg-white">
+
+    <!-- <div class="border rounded-md p-1 px-2 bg-white">
       <div class="flex row-auto gap-2">
         <p class="font-bold text-blue-400">Mobis Valium</p>
         <div
@@ -22,6 +32,7 @@
       </div>
       <p class="text-xs leading-6">Account ID : 63531691</p>
     </div>
+
     <div class="border rounded-md p-1 px-2 bg-white">
       <div class="flex row-auto gap-2">
         <p class="font-bold text-blue-400">Jereme Hunt 3</p>
@@ -32,12 +43,18 @@
         </div>
       </div>
       <p class="text-xs leading-6">Account ID : 63531691</p>
-    </div>
+    </div> -->
   </div>
 </template>
 
 <script>
 export default {
   name: "ContentTabs",
+  props: {
+    jsonData: {
+      type: Array,
+      required: true,
+    },
+  },
 };
 </script>
